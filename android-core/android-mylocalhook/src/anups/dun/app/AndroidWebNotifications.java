047468dde0cd6ee9ff72cbf941031e6b79cdc0d8
N.L.N.Rao <nellutlalnrao@gmail.com>
2018-04-30 17:04:47 +0530

    Fixed AlarmManager code at AndroidWebNotifications where it trigger only, if Alarm is not created.

--- android-core/android-mylocalhook/src/anups/dun/app/AndroidWebNotifications.java
+++ android-core/android-mylocalhook/src/anups/dun/app/AndroidWebNotifications.java
@@ -14,43 +14,55 @@ private Context context;
 /* ALARM: */
 public static PendingIntent auth_pendingIntent;
 public static AlarmManager auth_manager;
+
 public static PendingIntent version_pendingIntent; 
 public static AlarmManager version_manager;
-public static PendingIntent newsFeedImmediate_pendingIntent; 
-public static AlarmManager newsFeedImmediate_manager;
+
+public static PendingIntent latestNotifyService_pendingIntent; 
+public static AlarmManager latestNotifyService_manager;
+
 public static PendingIntent newsFeedInterval_pendingIntent; 
 public static AlarmManager newsFeedInterval_manager;
 
-public AndroidWebNotifications(Context c){
-	context = c; 
-}
-
-public void notify_versionupgrade(){
- Intent alarm_version = new Intent(context, VersionUpgradeAlarm.class);
- version_pendingIntent = PendingIntent.getBroadcast(context, 0, alarm_version, version_pendingIntent.FLAG_UPDATE_CURRENT);
- version_manager = (AlarmManager)context.getSystemService(context.getApplicationContext().ALARM_SERVICE);
- version_manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), AlarmManager.INTERVAL_HOUR, version_pendingIntent); // 60 minutes  
-}
+public AndroidWebNotifications(Context c){ context = c; }
 
 public void notify_authReminder(){
- Intent alarm_auth = new Intent(context, AuthenticationAlarm.class);
- auth_pendingIntent = PendingIntent.getBroadcast(context, 0, alarm_auth, auth_pendingIntent.FLAG_UPDATE_CURRENT);
- auth_manager = (AlarmManager)context.getSystemService(context.getApplicationContext().ALARM_SERVICE);
- auth_manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), AlarmManager.INTERVAL_HOUR, auth_pendingIntent); 
+ if(auth_pendingIntent==null) { /* Alarm is triggered only if it is previously not triggered */
+   Intent alarm_auth = new Intent(context, AuthenticationAlarm.class);
+   auth_pendingIntent = PendingIntent.getBroadcast(context, 0, alarm_auth, auth_pendingIntent.FLAG_UPDATE_CURRENT);
+   auth_manager = (AlarmManager)context.getSystemService(context.getApplicationContext().ALARM_SERVICE);
+   auth_manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), AlarmManager.INTERVAL_HOUR, auth_pendingIntent); 
+ }
 }
 
-public void notify_newsFeedImmediateReminder(){
- Intent alarm_nfi = new Intent(context, LatestNotificationServiceAlarm.class);
- newsFeedImmediate_pendingIntent = PendingIntent.getBroadcast(context, 0, alarm_nfi, newsFeedImmediate_pendingIntent.FLAG_UPDATE_CURRENT);
- newsFeedImmediate_manager = (AlarmManager)context.getSystemService(context.getApplicationContext().ALARM_SERVICE);
- newsFeedImmediate_manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1 * 60 * 1000, newsFeedImmediate_pendingIntent);  
+
+public void notify_versionupgrade(){
+ if(version_pendingIntent==null) { /* Alarm is triggered only if it is previously not triggered */
+   Intent alarm_version = new Intent(context, VersionUpgradeAlarm.class);
+   version_pendingIntent = PendingIntent.getBroadcast(context, 0, alarm_version, version_pendingIntent.FLAG_UPDATE_CURRENT);
+   version_manager = (AlarmManager)context.getSystemService(context.getApplicationContext().ALARM_SERVICE);
+   version_manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), AlarmManager.INTERVAL_HOUR, version_pendingIntent); // 60 minutes  
+ }
+}
+
+
+
+public void notify_latestNotificationService(){
+ if(latestNotifyService_pendingIntent==null) { /* Alarm is triggered only if it is previously not triggered */
+   Intent alarm_nfi = new Intent(context, LatestNotificationServiceAlarm.class);
+   latestNotifyService_pendingIntent = PendingIntent.getBroadcast(context, 0, alarm_nfi, latestNotifyService_pendingIntent.FLAG_UPDATE_CURRENT);
+   latestNotifyService_manager = (AlarmManager)context.getSystemService(context.getApplicationContext().ALARM_SERVICE);
+   latestNotifyService_manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1 * 60 * 1000, latestNotifyService_pendingIntent);  
+ }
 }
 
 public void notify_newsFeedIntervalReminder(){
- Intent alarm_nfi = new Intent(context, NewsFeedIntervalAlarm.class);
- newsFeedInterval_pendingIntent = PendingIntent.getBroadcast(context, 0, alarm_nfi, newsFeedInterval_pendingIntent.FLAG_UPDATE_CURRENT);
- newsFeedInterval_manager = (AlarmManager)context.getSystemService(context.getApplicationContext().ALARM_SERVICE);
- newsFeedInterval_manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), AlarmManager.INTERVAL_HOUR, newsFeedInterval_pendingIntent);  	
+ if(newsFeedInterval_pendingIntent==null) { /* Alarm is triggered only if it is previously not triggered */
+   Intent alarm_nfi = new Intent(context, NewsFeedIntervalAlarm.class);
+   newsFeedInterval_pendingIntent = PendingIntent.getBroadcast(context, 0, alarm_nfi, newsFeedInterval_pendingIntent.FLAG_UPDATE_CURRENT);
+   newsFeedInterval_manager = (AlarmManager)context.getSystemService(context.getApplicationContext().ALARM_SERVICE);
+   newsFeedInterval_manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), AlarmManager.INTERVAL_HOUR, newsFeedInterval_pendingIntent);  	
+ }
 }
 
 
