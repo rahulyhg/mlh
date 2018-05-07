e12fa6627ff50c0034db45ef4b97048ac92dbde0
N.L.N.Rao <nellutlalnrao@gmail.com>
2018-05-08 02:40:33 +0530

    Initialized ui-template api in Javascript

---
+++ android-web/js/api/ui-templates.js
@@ -0,0 +1,24 @@
+/* User with close button icon Function */
+function uiTemplate_userDisplayWithCloseButton(){
+
+}
+
+/* User without close button icon Function */
+function uiTemplate_userDisplayWithoutCloseButton(){
+
+}
+
+/* NewsFeed Display Function */
+function uiTemplate_newsFeedDisplay(){
+
+}
+
+/* Community Display Function */
+function uiTemplate_communityDisplay(){
+
+}
+
+/* Movement Display Function */
+function uiTemplate_movementDisplay(){
+
+}
