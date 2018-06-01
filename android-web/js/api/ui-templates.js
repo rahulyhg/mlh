1661f3c3205789a81fe009a99cda15e218dee251
N.L.N.Rao <nellutlalnrao@gmail.com>
2018-06-01 18:39:00 +0530

    Formated UI Template

--- android-web/js/api/ui-templates.js
+++ android-web/js/api/ui-templates.js
@@ -2,51 +2,6 @@
  * ================================== PEOPLE MANAGEMENT : START ==============================================================
  * ===========================================================================================================================
  */
-/* User with close button icon Function */
-function uiTemplate_displayPeople_viewMeButton(){
-  var content='<button class="btn btn-default custom-font m1 pull-right" ';
-	  content+='style="color:'+CURRENT_DARK_COLOR+';font-size:11px;">';
-	  content+='<b><i class="fa fa-user" aria-hidden="true"></i>&nbsp;Me</b></button>';
-  return content;
-}
-function uiTemplate_displayPeople_acceptRelationship(){
-  var content='<button class="btn custom-bg custom-font m1 pull-right" ';
-	  content+='style="background-color:'+CURRENT_DARK_COLOR+';color:#fff;font-size:11px;">';
-	  content+='<i class="fa fa-user" aria-hidden="true"></i>&nbsp;<b>Accept Friendship</b></button>';
-  return content;
-}
-function uiTemplate_displayPeople_reqSentDelRequest(param_del_userId){
-  var content='<div class="btn-group pull-right">';
-	  content+='<button class="btn btn-default custom-font" style="color:'+CURRENT_DARK_COLOR+';font-size:11px;">';
-	  content+='<b><i class="fa fa-check" aria-hidden="true"></i>&nbsp;Request Sent</b></button>';
-	  content+='<button class="btn custom-bg white-font" style="background-color:'+CURRENT_DARK_COLOR+';';
-	  content+='color:#fff;font-size:11px;" onclick="javascript:deleteARequestSent(\''+param_del_userId+'\')"><b>Delete Request</b>&nbsp;';
-      content+='<i class="fa fa-close" aria-hidden="true"></i></button>';
-	  content+='</div>';
-  return content;
-}
-function uiTemplate_displayPeople_sendReqHidePeople(param_userId){
- var content='<div class="btn-group pull-right">';
-	 content+='<button class="btn custom-bg white-font" style="background-color:'+CURRENT_DARK_COLOR+';color:#fff;font-size:11px;" ';
-	 content+='onclick="javascript:send_friend_request(\''+param_userId+'\')">';
-	 content+='<i class="fa fa-user" aria-hidden="true"></i>&nbsp;<b>Send Friend Request</b></button>';
-	 content+='<button class="btn custom-lgt-bg custom-font" style="background-color:'+CURRENT_LIGHT_COLOR+';font-size:11px;" ';
-	 content+='onclick="javascript:search_hide_currentPerson(\'searchpeople_'+param_userId+'\');">';
-	 content+='<b>Hide</b>&nbsp;<i class="fa fa-close" aria-hidden="true"></i></button>';
-	 content+='</div>';
- return content;
-}
-function uiTemplate_displayPeople_frndUnFrnd(param_userId){
- var content='<div class="btn-group pull-right">';
-	 content+='<button class="btn btn-default custom-font" style="color:'+CURRENT_DARK_COLOR+';font-size:11px;"><b>';
-	 content+='<i class="fa fa-check" aria-hidden="true"></i>&nbsp;Your Friend</b></button>';
-	 content+='<button class="btn custom-bg custom-font white-font" style="background-color:'+CURRENT_DARK_COLOR+';';
-	 content+='color:#fff;font-size:11px;" onclick="javascript:unfriendAperson(\''+param_userId+'\');">';
-	 content+='<b>UnFriend</b>&nbsp;<i class="fa fa-close" aria-hidden="true"></i></button>';
-	 content+='</div>';
- return content;
-}
-
 function uiTemplate_displayPeople_WithFriendsNonFriendsDiff(param_userId, param_profilepic, param_surName, param_name,
  param_minlocation,param_location,param_state,param_country,param_isFriend,param_youRecFrndRequest,param_youSentfrndRequest){
  var content='<div id="searchpeople_'+param_userId+'" class="list-group-item" style="padding:5px 0px;">';
@@ -79,16 +34,61 @@ function uiTemplate_displayPeople_WithFriendsNonFriendsDiff(param_userId, param_
  return content;
 }
 
+/* User with close button icon Function */
+function uiTemplate_displayPeople_viewMeButton(){
+  var content='<button class="btn btn-default custom-font m1 pull-right" ';
+	  content+='style="color:'+CURRENT_DARK_COLOR+';font-size:11px;">';
+	  content+='<b><i class="fa fa-user" aria-hidden="true"></i>&nbsp;Me</b></button>';
+  return content;
+}
+function uiTemplate_displayPeople_acceptRelationship(){
+  var content='<button class="btn custom-bg custom-font m1 pull-right" ';
+	  content+='style="background-color:'+CURRENT_DARK_COLOR+';color:#fff;font-size:11px;">';
+	  content+='<i class="fa fa-user" aria-hidden="true"></i>&nbsp;<b>Accept Friendship</b></button>';
+  return content;
+}
+function uiTemplate_displayPeople_reqSentDelRequest(param_del_userId){
+  var content='<div class="btn-group pull-right">';
+	  content+='<button class="btn btn-default custom-font" style="color:'+CURRENT_DARK_COLOR+';font-size:11px;">';
+	  content+='<b><i class="fa fa-check" aria-hidden="true"></i>&nbsp;Request Sent</b></button>';
+	  content+='<button class="btn custom-bg white-font" style="background-color:'+CURRENT_DARK_COLOR+';';
+	  content+='color:#fff;font-size:11px;" onclick="javascript:delete_relationship_request(\''+param_del_userId+'\')"><b>Delete Request</b>&nbsp;';
+      content+='<i class="fa fa-close" aria-hidden="true"></i></button>';
+	  content+='</div>';
+  return content;
+}
+function uiTemplate_displayPeople_sendReqHidePeople(param_userId){
+ var content='<div class="btn-group pull-right">';
+	 content+='<button class="btn custom-bg white-font" style="background-color:'+CURRENT_DARK_COLOR+';color:#fff;font-size:11px;" ';
+	 content+='onclick="javascript:send_relationship_request(\''+param_userId+'\')">';
+	 content+='<i class="fa fa-user" aria-hidden="true"></i>&nbsp;<b>Send Friend Request</b></button>';
+	 content+='<button class="btn custom-lgt-bg custom-font" style="background-color:'+CURRENT_LIGHT_COLOR+';font-size:11px;" ';
+	 content+='onclick="javascript:search_hide_currentPerson(\'searchpeople_'+param_userId+'\');">';
+	 content+='<b>Hide</b>&nbsp;<i class="fa fa-close" aria-hidden="true"></i></button>';
+	 content+='</div>';
+ return content;
+}
+function uiTemplate_displayPeople_frndUnFrnd(param_userId){
+ var content='<div class="btn-group pull-right">';
+	 content+='<button class="btn btn-default custom-font" style="color:'+CURRENT_DARK_COLOR+';font-size:11px;"><b>';
+	 content+='<i class="fa fa-check" aria-hidden="true"></i>&nbsp;Your Friend</b></button>';
+	 content+='<button class="btn custom-bg custom-font white-font" style="background-color:'+CURRENT_DARK_COLOR+';';
+	 content+='color:#fff;font-size:11px;" onclick="javascript:unfriendAperson(\''+param_userId+'\');">';
+	 content+='<b>UnFriend</b>&nbsp;<i class="fa fa-close" aria-hidden="true"></i></button>';
+	 content+='</div>';
+ return content;
+}
+
 /* One User Sending Request to Other User */
-function send_friend_request(user_Id){
+function send_relationship_request(user_Id){
  document.getElementById("searchpeople_btnsView_"+user_Id).innerHTML=uiTemplate_displayPeople_reqSentDelRequest(user_Id);
  js_ajax("GET",PROJECT_URL+'backend/php/dac/controller.page.app.findfriends.php',
  {action:'SEND_USERFRND_REQUESTS', from_user_Id:AUTH_USER_ID, to_user_Id:user_Id },function(resp){
   console.log(resp);
  });
 }
-
-function deleteARequestSent(user_Id){
+/* Delete a Request that previously Sent */
+function delete_relationship_request(user_Id){
  document.getElementById("searchpeople_btnsView_"+user_Id).innerHTML=uiTemplate_displayPeople_sendReqHidePeople(user_Id);
 js_ajax("GET",PROJECT_URL+'backend/php/dac/controller.page.app.findfriends.php',
 {action:'DELETE_A_REQUEST_SENT',from_userId:AUTH_USER_ID, to_userId:user_Id },function(resp){
@@ -96,9 +96,7 @@ js_ajax("GET",PROJECT_URL+'backend/php/dac/controller.page.app.findfriends.php',
 });
 }
 
-function search_hide_currentPerson(id){
-  $('#'+id).hide(1000);
-}
+function search_hide_currentPerson(id){ $('#'+id).hide(1000); }
 
 function uiTemplate_userDisplayWithCloseButton(){
 
