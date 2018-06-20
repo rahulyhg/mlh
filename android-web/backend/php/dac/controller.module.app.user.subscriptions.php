182727219c4d83878127cb67e9fa4f689b0b8864
N.L.N.Rao <nellutlalnrao@gmail.com>
2018-06-20 23:22:11 +0530

    Updated User Subscription code for Categories and SubCategories

--- android-web/backend/php/dac/controller.module.app.user.subscriptions.php
+++ android-web/backend/php/dac/controller.module.app.user.subscriptions.php
@@ -13,36 +13,25 @@ if(isset($_GET["action"])){
   if(isset($_GET["user_Id"])){
    $user_Id=$_GET["user_Id"];
    $subObj=new app_subscription();
-   $query=$subObj->query_getListOfSubscriptions($user_Id);
    $dbObj=new Database();
-   $jsonData=$dbObj->getJSONData($query);
-	// echo $jsonData.'<br/>';
-	$dejsonData1=json_decode($jsonData);
-	$content='{"domains" :[';
-	for($i1=0;$i1<count($dejsonData1);$i1++){
-	  $domain_Id=$dejsonData1[$i1]->{'domain_Id'};
-	  $domainName=$dejsonData1[$i1]->{'domainName'};
-	  $SubdomainList='['.$dejsonData1[$i1]->{'SubdomainList'}.']';
-	  $dejsonData2=json_decode($SubdomainList);
-	  $content.='{ "domain_Id" : "'.$domain_Id.'",';
+   $query01=$subObj->query_getCategoriesSubscription();
+   $jsonData01=$dbObj->getJSONData($query01);
+   $dejsonData01=json_decode($jsonData01);
+   $content='{"domains":[';
+   if(count($dejsonData01)>0){
+    for($i1=0;$i1<count($dejsonData01);$i1++){
+	  $domain_Id=$dejsonData01[$i1]->{'domain_Id'};
+	  $domainName=$dejsonData01[$i1]->{'domainName'};
+	  $content.='{"domain_Id":"'.$domain_Id.'",';
 	  $content.='"domainName":"'.$domainName.'",';
-	  $content.='"subscribed":"NO",';
-	  $content.='"subdomains":[';
-	  for($i2=0;$i2<count($dejsonData2);$i2++){
-		$subdomain_Id=$dejsonData2[$i2]->{'subdomain_Id'};
-		$subdomainName=$dejsonData2[$i2]->{'subdomainName'};
-		$subscribed=$dejsonData2[$i2]->{'subscribed'};
-		$content.='{ "subdomain_Id":"'.$subdomain_Id.'", ';
-		$content.='"subdomainName":"'.$subdomainName.'" ,';
-		$content.='"subscribed":"'.$subscribed.'" },';
-	  }
-	  $content=chop($content,',');
-	  $content.=']';
-	  $content.='},';
+	  $query02=$subObj->query_getSubCategoriesSubscription($user_Id,$domain_Id);
+	  $jsonData02=$dbObj->getJSONData($query02);
+	  $content.='"subdomains":'.$jsonData02.'},';
 	}
 	$content=chop($content,',');
-	$content.=']}';
-	echo $content;
+   }
+   $content.=']}';
+   echo $content;
   } 
   else { echo 'MISSING_USERID'; }
  }
--- android-web/backend/php/dal/data.module.app.user.subscriptions.php
+++ android-web/backend/php/dal/data.module.app.user.subscriptions.php
@@ -1,19 +1,21 @@
 <?php 
 class app_subscription {
- function query_getListOfSubscriptions($user_Id){
- /* Get List of Subscriptions from the Tables "subs_dom_info" and "subs_subdom_info" */
-  $sql="SELECT subs_dom_info.domain_Id, subs_dom_info.domainName, ";
-  $sql.="GROUP_CONCAT(CONCAT('{\"subdomain_Id\":\"',subs_subdom_info.subdomain_Id,'\",\"subdomainName\":\"', ";
-  $sql.="subs_subdom_info.subdomainName,'\",\"subscribed\":\"',";
-  $sql.="(SELECT IF((SELECT count(*) FROM user_subscription WHERE ";
-  $sql.=" user_subscription.domain_Id=subs_dom_info.domain_Id AND ";
-  $sql.="user_subscription.subdomain_Id=subs_subdom_info.subdomain_Id AND ";
-  $sql.="user_subscription.user_Id='".$user_Id."')>0,'YES','NO')), ";
-  $sql.="'\"}')) As SubdomainList ";
-  $sql.="FROM subs_dom_info, subs_subdom_info WHERE subs_dom_info.domain_Id=subs_subdom_info.domain_Id ";
-  $sql.="GROUP BY subs_dom_info.domain_Id; ";
-  return $sql;
+/* Execute SHOW VARIABLES; in Database and  SET SESSION group_concat_max_len = 1000000;
+ */
+ function query_getCategoriesSubscription(){
+   $sql="SELECT domain_Id, domainName FROM subs_dom_info; ";
+   return $sql;
  }
+ 
+ function query_getSubCategoriesSubscription($user_Id,$domain_Id){
+   $sql="SELECT subdomain_Id, subdomainName, ";
+   $sql.="(SELECT IF((SELECT count(*) FROM user_subscription WHERE user_subscription.user_Id='".$user_Id."'";
+   $sql.=" AND user_subscription.domain_Id='".$domain_Id."' AND ";
+   $sql.="user_subscription.subdomain_Id=subs_subdom_info.subdomain_Id)>0, 'YES','NO')) As subscribed ";
+   $sql.=" FROM subs_subdom_info WHERE domain_Id='".$domain_Id."';";
+   return $sql;
+ }
+ 
  function query_setUserSubscription($sub_Id,$user_Id,$domain_Id,$subdomain_Id){
  $sql="INSERT INTO user_subscription(sub_Id, user_Id, domain_Id, subdomain_Id, sub_on) ";
  $sql.="SELECT * FROM (SELECT '".$sub_Id."','".$user_Id."', '".$domain_Id."', '".$subdomain_Id."','";
