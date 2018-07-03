f0d444b9a713ae063659abe77459d6eb3d9c9e54
N.L.N.Rao <nellutlalnrao@gmail.com>
2018-07-03 20:21:16 +0530

    Updated Notification Services for Union Member Request Reciever / Sender.

--- android-web/backend/php/dal/data.module.app.notifications.php
+++ android-web/backend/php/dal/data.module.app.notifications.php
@@ -2,9 +2,11 @@
 class app_notifications {
 
   /***************************************************************************************************************************
-   * USER FRIENDSHIP NOTIFICATIONS */
+   ******************************************* USER FRIENDSHIP NOTIFICATIONS *************************************************
+   ***************************************************************************************************************************/
+  
   function query_notify_usrFrndsReqReciever($user_Id){
-  /* Notification Query to User as he recieved Friendship Request */
+  /* Notification Query to User recieved Friendship Request */
     $sql="SELECT ";
     $sql.=" user_account.surName, user_account.name, user_account.profile_pic, user_account.minlocation,";
     $sql.=" user_account.location, user_account.state, user_account.country, user_frnds_req.req_on, user_frnds_req.req_tz ";
@@ -26,7 +28,8 @@ class app_notifications {
    return $sql;
   }
   
-  function query_notify_unionMemberRequest($user_Id){
+  function query_notify_unionMemberReqReciever($user_Id){
+  /* Notification Query to User Recieved Membership Request for a Union */
     $sql="SELECT ";
 	$sql.=" subs_dom_info.domainName, subs_subdom_info.subdomainName, ";
 	$sql.=" user_account.surName, user_account.name, unionprof_account.union_Id, unionprof_account.unionName, ";
@@ -39,8 +42,31 @@ class app_notifications {
 	$sql.=" unionprof_account.domain_Id = subs_dom_info.domain_Id AND ";
 	$sql.=" unionprof_account.subdomain_Id = subs_subdom_info.subdomain_Id AND ";
 	$sql.=" unionprof_mem_req.req_to='".$user_Id."' AND unionprof_mem_req.req_from=user_account.user_Id; ";
+	return $sql;
   }
   
+  function query_notify_onAcceptUnionMemberReq($user_Id){
+  /* Notification Query to Union Member Joined and Added into Union */
+   $sql="SELECT unionprof_mem.member_Id, ";
+   $sql.="(SELECT ";
+   $sql.="CONCAT('[{\"union_Id\":\"',unionprof_mem.union_Id,'\",\"unionName\":\"',unionprof_account.unionName, ";
+   $sql.="'\",\"profile_pic\":\"',unionprof_account.profile_pic,'\"}]') ";
+   $sql.="FROM unionprof_account WHERE unionprof_account.union_Id=unionprof_mem.union_Id)  As unionInfo, ";
+   $sql.="(SELECT CONCAT('[{\"minlocation\":\"',unionprof_branch.minlocation,'\",\"location\":\"', ";
+   $sql.="unionprof_branch.location,'\",\"state\":\"',unionprof_branch.state,'\",\"country\":\"',unionprof_branch.country,";
+   $sql.="'\"}]') FROM unionprof_branch WHERE unionprof_branch.branch_Id=unionprof_mem.branch_Id) As branchInfo, ";
+   $sql.="(SELECT CONCAT('[{\"user_Id\":\"',user_account.user_Id,'\",\"username\":\"',user_account.username, ";
+   $sql.="'\",\"surName\":\"',user_account.surName,'\",\"name\":\"',user_account.name,'\",\"memAddedOn\":\"',";
+   $sql.="unionprof_mem.memAddedOn,'\"}]') FROM user_account WHERE user_account.user_Id=unionprof_mem.user_Id) As ";
+   $sql.="memberJoinedInfo, (SELECT unionprof_mem_roles.cur_roleName FROM unionprof_mem_roles WHERE ";
+   $sql.="unionprof_mem_roles.role_Id=unionprof_mem.cur_role_Id) As roleName, unionprof_mem.cur_role_Id, ";
+   $sql.="(SELECT CONCAT('[{\"user_Id\":\"',user_account.user_Id,'\",\"username\":\"',user_account.username, ";
+   $sql.="'\",\"surName\":\"',user_account.surName,'\",\"name\":\"',user_account.name,'\"}]') ";
+   $sql.="FROM user_account WHERE user_account.user_Id=unionprof_mem.memAddedBy) As memberJoinedByInfo ";
+   $sql.="FROM unionprof_mem WHERE unionprof_mem.user_Id='".$user_Id."' OR unionprof_mem.memAddedBy='".$user_Id."' ";
+   $sql.="AND unionprof_mem.memNotify='Y';";
+   return $sql;
+  }
   
 }
 ?>
