072db8f870e8b2e36bffbdddbc65ede64a8ac6cd
N.L.N.Rao <nellutlalnrao@gmail.com>
2018-07-30 23:17:20 +0530

    Added Test code for Service Dump Contacts in backendValidator

---
+++ android-test/backendValidator/src/anups/dun/mlh/basic/services/AppAndroidService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anups.dun.mlh.basic.services;
+
+import anups.dun.mlh.basic.url.AppAndroidServiceURL;
+import anups.dun.utils.URLRequestService;
+
+/**
+ * POST SERVICE
+ * @author N.L.N.Rao
+ */
+public class AppAndroidService {
+  public String userDumpFriendsService(){ /* SERVICE_USRDUMPFRNDS*/
+    AppAndroidServiceURL appAndroidServiceURL = new AppAndroidServiceURL();
+    URLRequestService urlRequestService = new URLRequestService();
+    String response = urlRequestService.sendPostRequest(AppAndroidServiceURL.SERVICE_URL, appAndroidServiceURL.userDumpFriendsServiceParameters());
+    return response;
+  }
+  
+  public static void main(String args[]){
+    System.out.println(new AppAndroidService().userDumpFriendsService());
+  }
+}
---
+++ android-test/backendValidator/src/anups/dun/mlh/basic/url/AppAndroidServiceURL.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anups.dun.mlh.basic.url;
+
+import anups.dun.mlh.basic.data.AppAndroidServiceData;
+
+/**
+ *
+ * @author N.L.N.Rao
+ */
+public class AppAndroidServiceURL {
+  public static final String SERVICE_URL="http://localhost/mlh-backend/backend/php/dac/controller.module.app.android.service.php";
+  public String userDumpFriendsServiceParameters(){
+     AppAndroidServiceData appAndroidServiceData = new AppAndroidServiceData();
+     AppAndroidServiceData.UserDumpFriendsService userDumpFriendsService = appAndroidServiceData.new UserDumpFriendsService();
+     StringBuilder params = new StringBuilder();
+     params.append("action=").append("SERVICE_USRDUMPFRNDS").append("&");
+     params.append("user_Id=").append(userDumpFriendsService.USER_ID).append("&");
+     params.append("phoneNumbers=[");
+     for(int index=0;index<userDumpFriendsService.PHONENUMBERS.length;index++){
+       params.append(userDumpFriendsService.PHONENUMBERS[index].toString()).append(",");
+     }
+     params.deleteCharAt(params.length()-1);
+     params.append("]");
+     return params.toString();
+  }
+  
+}
