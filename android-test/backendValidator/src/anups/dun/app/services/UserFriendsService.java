3824d31f3bcbddd96b5f920c84d84fafdf76df4c
N.L.N.Rao <nellutlalnrao@gmail.com>
2018-08-18 19:27:19 +0530

    Updated android-test backendValidator Code

--- android-test/backendValidator/src/anups/dun/app/services/UserFriendsService.java
+++ android-test/backendValidator/src/anups/dun/app/services/UserFriendsService.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package anups.dun.mlh.basic.services;
+package anups.dun.app.services;
 
 import anups.dun.mlh.basic.url.UserFriendsServiceURL;
 import anups.dun.utils.URLRequestService;
@@ -13,11 +13,16 @@ import anups.dun.utils.URLRequestService;
  * @author N.L.N.Rao
  */
 public class UserFriendsService {
-    
+  public static final String SERVICE_URL="http://localhost/mlh/android-web/backend/php/dac/controller.module.app.user.friends.php";
+  
   public String sendUserFrndRequests(){ /* SEND_USERFRND_REQUESTS */
-    UserFriendsServiceURL userFriendsServiceURL = new UserFriendsServiceURL();
+      StringBuilder urlWithparams = new StringBuilder();
+                    urlWithparams.append(SERVICE_URL).append("?action=SEND_USERFRND_REQUESTS&");
+                    urlWithparams.append("projectURL=http://localhost/mlh/android-web/&");
+                    urlWithparams.append("from_user_Id=USR255798352927&");
+                    urlWithparams.append("to_user_Id=USR113561617186");
     URLRequestService urlRequestService = new URLRequestService();
-    String response = urlRequestService.sendGetRequest(userFriendsServiceURL.sendUserFrndRequestsParameters());
+    String response = urlRequestService.sendGetRequest(urlWithparams.toString());
     return response;
   }
     
