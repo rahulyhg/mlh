a0f6b8850f28388fc16d4b6813db7f470aeee205
N.L.N.Rao <nellutlalnrao@gmail.com>
2018-09-23 23:19:00 +0530

    Update Create Branch Code

--- android-web/app-community-profile-createNewBranch.php
+++ android-web/app-community-profile-createNewBranch.php
@@ -17,6 +17,7 @@ if(isset($_SESSION["AUTH_USER_ID"])) {
  <link rel="stylesheet" href="<?php echo $_SESSION["PROJECT_URL"]; ?>styles/api/jquery-ui.css"> 
  <link rel="stylesheet" type="text/css" href="<?php echo $_SESSION["PROJECT_URL"]; ?>styles/api/easy-autocomplete.min.css"/>
  <script src="<?php echo $_SESSION["PROJECT_URL"]; ?>js/api/jquery.min.js"></script>
+ <script src="<?php echo $_SESSION["PROJECT_URL"]; ?>js/api/db.identity.js"></script>
  <script src="<?php echo $_SESSION["PROJECT_URL"]; ?>js/api/core-skeleton.js"></script>
  <script src="<?php echo $_SESSION["PROJECT_URL"]; ?>js/api/bootstrap.min.js"></script>
  <script src="<?php echo $_SESSION["PROJECT_URL"]; ?>js/api/jquery-ui.js"></script>
@@ -142,25 +143,88 @@ autocomplete_load_members();
 var MEMBER_ID;
 var MEMBER_DISPLAYCONTENT;
 var MEMBER_ARRAY_ID=[];
+var MEMBER_ROLE_ARRAY=[];
 function deleteFromArray_member(id){
 
 }
-function addToArray_members(){
+function finish_AddMembersToBranch(){
+ console.log("MEMBER_ARRAY_ID: "+JSON.stringify(MEMBER_ARRAY_ID));
+ console.log("MEMBER_ROLE_ARRAY: "+JSON.stringify(MEMBER_ROLE_ARRAY));
+}
+
+function addRoleToMemberOfBranch(member_Id){
+ var role_Id = unionprof_mem_roles_Id();
+ var roleName = document.getElementById("roleName_"+member_Id).value;
+ MEMBER_ROLE_ARRAY[MEMBER_ROLE_ARRAY.length] = { role_Id: role_Id, roleName: roleName };
+ for(var index=0;index<MEMBER_ARRAY_ID.length;index++){
+   if(member_Id===MEMBER_ARRAY_ID[index].member_Id){
+    
+   }
+ }
  
+}
+
+function addToArray_members(){
+ document.getElementById("communityNewBranch_members_name").value="";
  console.log(MEMBER_DISPLAYCONTENT);
  var duplicateRecognizer=false;
  if(MEMBER_ARRAY_ID.length>0){
  for(var index=0;index<MEMBER_ARRAY_ID.length;index++){
-  if(MEMBER_ID===MEMBER_ARRAY_ID[index]) { duplicateRecognizer=true;break; }
+  if(MEMBER_ID===MEMBER_ARRAY_ID[index].member_Id) { duplicateRecognizer=true;break; }
  }
  }
  if(MEMBER_DISPLAYCONTENT!==undefined && !duplicateRecognizer){
    var size=MEMBER_ARRAY_ID.length;
-   MEMBER_ARRAY_ID[size]=MEMBER_ID;
+   MEMBER_ARRAY_ID[size]={ member_Id:MEMBER_ID };
    MEMBER_DISPLAYCONTENT+='<div id="div_communityNewBranch_selectedMembers'+(size+1)+'"></div>';
    document.getElementById("div_communityNewBranch_selectedMembers"+size).innerHTML=MEMBER_DISPLAYCONTENT;
+   autocomplete_load_memberRole(MEMBER_ID);
  }
  
+}
+function displayTemplate_membersContent(profile_pic, name, minlocation, location, state, country){
+    MEMBER_DISPLAYCONTENT='<div class="list-group">';
+	MEMBER_DISPLAYCONTENT+='<div class="list-group-item pad0">';
+	MEMBER_DISPLAYCONTENT+='<div class="container-fluid mtop15p mbot15p">';
+	MEMBER_DISPLAYCONTENT+='<div class="row">';
+	MEMBER_DISPLAYCONTENT+='<div class="col-xs-12">';
+	MEMBER_DISPLAYCONTENT+='<button type="button" class="close" data-dismiss="modal">&times;</button>';
+	MEMBER_DISPLAYCONTENT+='</div>';
+    MEMBER_DISPLAYCONTENT+='</div>'
+	MEMBER_DISPLAYCONTENT+='<div class="row">';
+	MEMBER_DISPLAYCONTENT+='<div class="col-xs-4">';
+	MEMBER_DISPLAYCONTENT+='<img src="' +profile_pic+ '" class="profile_pic_img3"/>';
+	MEMBER_DISPLAYCONTENT+='</div>';
+	MEMBER_DISPLAYCONTENT+='<div class="col-xs-8">';
+	MEMBER_DISPLAYCONTENT+='<div>'+name+'<div>';
+	MEMBER_DISPLAYCONTENT+='<div class="mtop5p">';
+	MEMBER_DISPLAYCONTENT+='<div class="input-group">';
+	MEMBER_DISPLAYCONTENT+='<input id="roleName_'+MEMBER_ID+'" type="text" style="z-index:0;" class="form-control" placeholder="Enter Role">';
+	MEMBER_DISPLAYCONTENT+='<span id="roleDone_'+MEMBER_ID+'" class="input-group-addon" ';
+	MEMBER_DISPLAYCONTENT+='onclick="javascript:addRoleToMemberOfBranch(\''+MEMBER_ID+'\');"><b>Done</b></span>';
+	MEMBER_DISPLAYCONTENT+='</div>';
+	MEMBER_DISPLAYCONTENT+='</div>';
+	MEMBER_DISPLAYCONTENT+='<div class="mtop5p" style="color:#929090;">'+minlocation+', '+location+', ';
+	MEMBER_DISPLAYCONTENT+=state+', '+country+'</div>';
+	MEMBER_DISPLAYCONTENT+='</div>';
+    MEMBER_DISPLAYCONTENT+='</div>';
+	MEMBER_DISPLAYCONTENT+='</div>';
+	MEMBER_DISPLAYCONTENT+='</div>';
+	MEMBER_DISPLAYCONTENT+='</div>';
+	MEMBER_DISPLAYCONTENT+='</div>';
+	MEMBER_DISPLAYCONTENT+='</div>';
+}
+function autocomplete_load_memberRole(MEMBER_ID){
+    $("#roleName_"+MEMBER_ID).autocomplete({        
+        source: function(request, response) {
+		    console.log("autocomplete_load_memberRole: "+JSON.stringify(MEMBER_ROLE_ARRAY));
+		    response(MEMBER_ROLE_ARRAY);
+        },
+        select: function( event, ui ) {
+              $("#roleName_"+MEMBER_ID).val( ui.item.roleName); //ui.item is your object from the array
+          return false;
+        }
+    });
 }
 function autocomplete_load_members(){
 var options = { url: function(phrase) {
@@ -177,27 +241,7 @@ var options = { url: function(phrase) {
 			 var location = $("#communityNewBranch_members_name").getSelectedItemData().location;
 			 var state = $("#communityNewBranch_members_name").getSelectedItemData().state;
 			 var country = $("#communityNewBranch_members_name").getSelectedItemData().country;
-			 MEMBER_DISPLAYCONTENT='<div class="list-group">';
-			 MEMBER_DISPLAYCONTENT+='<div class="list-group-item pad0">';
-			 MEMBER_DISPLAYCONTENT+='<div class="container-fluid mtop15p mbot15p">';
-			 MEMBER_DISPLAYCONTENT+='<div class="row">';
-			 MEMBER_DISPLAYCONTENT+='<div class="col-xs-12">';
-			 MEMBER_DISPLAYCONTENT+='<button type="button" class="close" data-dismiss="modal">&times;</button>';
-			 MEMBER_DISPLAYCONTENT+='</div>';
-             MEMBER_DISPLAYCONTENT+='</div>';
-			 MEMBER_DISPLAYCONTENT+='<div class="row">';
-			 MEMBER_DISPLAYCONTENT+='<div class="col-xs-4">';
-			 MEMBER_DISPLAYCONTENT+='<img src="' +profile_pic+ '" class="profile_pic_img3"/>';
-			 MEMBER_DISPLAYCONTENT+='</div>';
-			 MEMBER_DISPLAYCONTENT+='<div class="col-xs-8">';
-			 MEMBER_DISPLAYCONTENT+='<span>'+name+'<span><br/>';
-			 MEMBER_DISPLAYCONTENT+='<span style="color:#929090;">'+minlocation+', '+location+', ';
-			 MEMBER_DISPLAYCONTENT+=state+', '+country+'</span>';
-			 MEMBER_DISPLAYCONTENT+='</div>';
-             MEMBER_DISPLAYCONTENT+='</div>';
-			 MEMBER_DISPLAYCONTENT+='</div>';
-			 MEMBER_DISPLAYCONTENT+='</div>';
-			 MEMBER_DISPLAYCONTENT+='</div>';
+			 displayTemplate_membersContent(profile_pic, name, minlocation, location, state, country);
 		   },
 		   onHideListEvent: function() { console.log(MEMBER_ID); }
 	},
@@ -235,6 +279,11 @@ $(".easy-autocomplete-container").css('margin-top','30px');
 <div class="row">
 <div class="col-xs-12">
 <div id="div_communityNewBranch_selectedMembers0"></div>
+</div>
+</div>
+<div class="row">
+<div class="col-xs-12">
+<button class="btn btn-default" onclick="javascript:finish_AddMembersToBranch();"><b>I'm done</b></button>
 </div>
 </div>
 </div>
